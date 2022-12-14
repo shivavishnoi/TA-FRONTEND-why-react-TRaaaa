6740c128ce5a78ca218eaf584c5914edb3cf4aa0
shivaji <shivavishnoi.vishnoi@gmail.com>
2022-12-14 15:15:56 +0530

    Gizmo - basic react app

---
+++ block-BRaaao/code/gizmo/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header(props) {
+  return (
+    <>
+      <nav>
+        <img src={'vite.svg'} />
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
