e8439e28b00915c5714337b38616e39c659de0e0
shivaji <shivavishnoi.vishnoi@gmail.com>
2022-12-14 08:41:50 +0530

    making react components

---
+++ block-BRaaam/code/basic/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(props) {
+  return (
+    <>
+      <ul>
+        <li>Policies</li>
+        <li>Advertise</li>
+        <li>Developer tools</li>
+      </ul>
+    </>
+  );
+}
+
+export default Footer;
